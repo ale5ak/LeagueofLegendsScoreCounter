fd7fbee228a3c4e0b8343e861b67ef40d784a11b
Aleš Pros <Ale Pros>
2017-03-17 11:18:31 +0100

    Buttons were put into the array for better readibility

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -28,7 +28,7 @@ import android.widget.Toast;
 /**
  * This activity keeps track of the basketball score for 2 teams.
  */
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     Team teamA = new Team(R.id.team_a_score);
     Team teamB = new Team(R.id.team_b_score);
@@ -57,26 +57,29 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             teamB.addScore(savedInstanceState.getInt(STATE_B_SCORE));
         }
 
-        Button add3forA = (Button) findViewById(R.id.add3forA);
-        add3forA.setOnClickListener(this); // calling onClick() method
+        Button[] buttons = new Button[6];
+        buttons[0] = (Button) findViewById(R.id.add3forA);
+        buttons[1] = (Button) findViewById(R.id.add2forA);
+        buttons[2] = (Button) findViewById(R.id.add1forA);
+        buttons[3] = (Button) findViewById(R.id.add3forB);
+        buttons[4] = (Button) findViewById(R.id.add2forB);
+        buttons[5] = (Button) findViewById(R.id.add1forB);
 
-        Button add2forA = (Button) findViewById(R.id.add2forA);
-        add2forA.setOnClickListener(this); // calling onClick() method
+        for (Button button : buttons) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String buttonName = getResources().getResourceEntryName(v.getId());
+                    int points = Integer.parseInt(buttonName.substring(3, 4));
 
-        Button add1forA = (Button) findViewById(R.id.add1forA);
-        add1forA.setOnClickListener(this); // calling onClick() method
-
-
-        Button add3forB = (Button) findViewById(R.id.add3forB);
-        add3forB.setOnClickListener(this); // calling onClick() method
-
-        Button add2forB = (Button) findViewById(R.id.add2forB);
-        add2forB.setOnClickListener(this); // calling onClick() method
-
-        Button add1forB = (Button) findViewById(R.id.add1forB);
-        add1forB.setOnClickListener(this); // calling onClick() method
-
-
+                    if (buttonName.substring(7, 8).equals("A")) {
+                        teamA.addScore(points);
+                    } else {
+                        teamB.addScore(points);
+                    }
+                }
+            });
+        }
     }
 
     /**
@@ -85,19 +88,6 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     public void resetScore(View v) {
         teamA.resetScore();
         teamB.resetScore();
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        String buttonName = getResources().getResourceEntryName(v.getId());
-        int points = Integer.parseInt(buttonName.substring(3, 4));
-
-        if (buttonName.substring(7, 8).equals("A")) {
-            teamA.addScore(points);
-        } else {
-            teamB.addScore(points);
-        }
     }
 
     public class Team {
